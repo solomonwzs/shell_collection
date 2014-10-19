47d03b30fa3855f9fc4cc8a9e2a711b742bd55b0
Solomon Wu <solomon.wzs@gmail.com>
2014-10-20 01:22:31 +0800

    modified:   fifo_test.sh

--- fifo_test.sh
+++ fifo_test.sh
@@ -1,30 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 
-N=5
+N=20
+FINISH=0
+IDLE=3
 FIFO=/tmp/$$.fifo
 
 mkfifo $FIFO
-#exec 3<>$FIFO
+exec 3<>$FIFO
+trap "rm $FIFO; exit" SIGINT SIGTERM
 
-loop(){
-    for i in $(seq 1 $N); do
-        until read tmp; do
-            :
-        done
-        #read -u3 tmp
-        echo "$tmp out"
-    done < $FIFO
-}
-
-loop &
-LOOP_PID=$!
 for i in $(seq 1 $N); do
-    sleep 0.1; echo $i > $FIFO
-    #sleep 0.1; echo $i >&3
-    echo "$i in"
+    echo "task $i">&3
 done
-wait $LOOP_PID
+
+while [ "$FINISH" -lt "$N" ]; do
+    read -u3 tag args
+
+    if [ "$tag" == "task" ] && [ "$IDLE" -gt 0 ]; then
+        IDLE=$(($IDLE-1))
+        {
+            sleep 0.2
+            echo "$args ok"
+            echo "ok -" >&3
+        } &
+    elif [ "$tag" == "task" ] && [ "$IDLE" -eq 0 ]; then
+        echo "task $args" >&3
+    elif [ "$tag" == "ok" ]; then
+        IDLE=$(($IDLE+1))
+        FINISH=$(($FINISH+1))
+    fi
+done
+
+wait
+
+exec 3>&-
+exec 3<&-
+rm $FIFO
 
 echo "end"
-
-rm $FIFO
